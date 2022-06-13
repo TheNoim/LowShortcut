e8c80e9549b1faa7eeabca742751b45ffbf0e42d
Nils Bergmann <nilsbergmann@noim.io>
2022-06-13 11:35:53 +0200

    Initial Commit

---
+++ LowShortcut/LowShortcutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LowShortcutApp.swift
+//  LowShortcut
+//
+//  Created by Nils Bergmann on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LowShortcutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
